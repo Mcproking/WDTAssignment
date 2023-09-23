0c9f48c42ac32bcc826e885ee9ea3071f63fb509
TheJK <shanefoomg@gmail.com>
2023-09-23 16:40:45 +0800

    init

---
+++ index.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Name Plsssss</title>
+    <link rel="stylesheet" href="./static/body.css">
+    <link rel="stylesheet" href="./static/header.css">
+    <link rel="stylesheet" href="./static/navbar.css">
+    <link rel="stylesheet" href="./static/index.css">
+</head>
+<body>
+    <div id="warper">
+        <?php 
+        include 'includes/header.html';
+        include 'includes/navbar.html';?>
+
+        <div id="scrollimg">
+
+        </div>
+        <div id="about">
+
+        </div>
+        <div id="latest">
+
+        </div>
+        <div id="category">
+
+        </div>
+    </div>
+</body>
+</html>
