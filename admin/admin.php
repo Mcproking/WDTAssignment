be583e9c5c61e5f4e904c4ab0725988621be0441
Dabking228 <darylsim228@gmail.com>
2023-10-18 17:05:10 +0800

    done admin.php dashboard

--- admin/admin.php
+++ admin/admin.php
@@ -9,18 +9,22 @@
     <link rel="stylesheet" href="../static/admin/admin.css">
 </head>
 <body>
-    <div id="wrapper">
-        <div id="stats">
-            <?php
-                include '../includes/header.php';
-                include '../php/db_conn.php';
-                $queryCustomer = "SELECT COUNT(CustomerID) FROM Customer GROUP BY CustomerID";
-                $querySeller = "SELECT COUNT(SellerID) FROM Customer GROUP BY SellerID";
-                $queryItem = "SELECT COUNT(ItemID) FROM Customer GROUP BY ItemID";
-            ?>
+    <?php
+        include '../includes/header.php';
+        include '../php/db_conn.php';
+        $queryCustomer = "SELECT COUNT(CustomerID) FROM Customer GROUP BY CustomerID";
+        $querySeller = "SELECT COUNT(SellerID) FROM Customer GROUP BY SellerID";
+        $queryItem = "SELECT COUNT(ItemID) FROM Customer GROUP BY ItemID";
+    ?>
 
+    <div id="wrapper">
+        <div id="title" align="center">
+            <h1>Admin Dashboard</h1>
+        </div>
+
+        <div id="stats">
             <div id="customers" class="box">
-                <p><img src="../imgs/customer.png" alt="Customer" height="50" width="50" id="imgCustomer" class="image">Total Number Of     Customers: <?php echo $queryCustomer;?></p>
+                <p><img src="../imgs/customer.png" alt="Customer" height="50" width="50" id="imgCustomer" class="image">Total Number Of Customers: <?php echo $queryCustomer;?></p>
             </div>
 
             <div id="sellers" class="box">
@@ -31,13 +35,15 @@
                 <p><img src="../imgs/item1.png" alt="Item" height="50" width="50" id="imgItem" class="image">Total Number Of Items:<?php echo   $queryItem; ?></p>
             </div>
         </div>
-            
 
         <div id="options">
-            <button onclick="window.location.href='manageUser.php'" id="users">Manage Users</button>
-            <button onclick="window.location.href='manageItem.php'" id="items">Manage Items</button>
+            <button onclick="window.location.href='manageUser.php'" id="users" class="Button">Manage Users</button>
+            <button onclick="window.location.href='manageItem.php'" id="items" class="Button">Manage Items</button>
         </div>
     </div>
-
+    
+    <?php
+        include './includes/footer.html';
+    ?>
 </body>
 </html>
