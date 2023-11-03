52e55e2f0624c359edbcef8963579edbda29081f
Dabking228 <darylsim228@gmail.com>
2023-11-04 01:34:55 +0800

    seller list structure done

--- admin/sellerList.php
+++ admin/sellerList.php
@@ -26,12 +26,13 @@
     </div>
 
     <div id="items_title">
-        <h1>Items</h1>
+        <h1>Seller List</h1>
     </div>
 
     <div>
         <form action="" method="get" id="Search">
             Search by Username: <input type="text" name="searchSeller" id="searchSeller" placeholder="Enter Username">
+            <button type="submit" id="searchButton" name="searchButton">Search</button>
         </form>
     </div>
 
@@ -55,3 +56,88 @@
 </body>
 </html>
 
+<?php
+    include "../php/db_conn.php";
+    
+    $displayQuery = "SELECT * FROM Seller ORDER BY SellerID ASC LIMIT 20";
+    $displayResults = mysqli_query($conn, $displayQuery);
+    
+    if (isset($_GET['searchSeller'])) {
+        $username = $_GET['searchSeller'];
+    }
+    $searchQuery = "SELECT * FROM Seller WHERE SellerUsername = '$username'";
+    $searchResults =  mysqli_query($conn, $searchResults);
+
+    if (mysqli_num_rows($searchResults) > 0) {
+?>
+        <table border="1">
+            <tr>
+                <th>Seller ID</th>
+                <th>Username</th>
+                <th>Email</th>
+                <th>Phone</th>
+                <th>Address</th>
+                <th>State</th>
+                <th colspan="2">Actions</th>
+            </tr>
+
+        <?php
+        while ($row = mysqli_fetch_assoc($searchResults)) {
+            echo "<tr>";
+            echo "<td>" . $row['SellerID'] . "</td>";
+            echo "<td>" . $row['SellerUsername'] . "</td>";
+            echo "<td>" . $row['SellerEmail'] . "</td>";
+            echo "<td>" . $row['SellerPhone'] . "</td>";
+            echo "<td>" . $row['SellerAddress'] . "</td>";
+            echo "<td>" . $row['SellerState'] . "</td>";
+        ?>
+            <td><a class="actionButtons" href="editSeller.php?SellerID=<?php echo $row['SellerID']; ?>">Edit</a></td>
+            <td><a class="actionButtons" href="deleteSeller.php?SellerID=<?php echo $row['SellerID']; ?>">Delete</a></td>
+
+        <?php
+            echo "</tr>";
+        }
+    }
+    else {
+        echo "No records found.";
+    }
+?>
+        </table>
+
+      
+<?php    
+    if (mysqli_num_rows($displayResults) > 0) {
+?>
+        <table border="1">
+            <tr>
+                <th>Seller ID</th>
+                <th>Username</th>
+                <th>Email</th>
+                <th>Phone</th>
+                <th>Address</th>
+                <th>State</th>
+                <th colspan="2">Actions</th>
+            </tr>
+
+        <?php
+        while ($row = mysqli_fetch_assoc($displayResults)) {
+            echo "<tr>";
+            echo "<td>" . $row['SellerID'] . "</td>";
+            echo "<td>" . $row['SellerUsername'] . "</td>";
+            echo "<td>" . $row['SellerEmail'] . "</td>";
+            echo "<td>" . $row['SellerPhone'] . "</td>";
+            echo "<td>" . $row['SellerAddress'] . "</td>";
+            echo "<td>" . $row['SellerState'] . "</td>";
+        ?>
+            <td><a class="actionButtons" href="editSeller.php?SellerID=<?php echo $row['SellerID']; ?>">Edit</a></td>
+            <td><a class="actionButtons" href="deleteSeller.php?SellerID=<?php echo $row['SellerID']; ?>">Delete</a></td>
+
+        <?php
+            echo "</tr>";
+        }
+    }
+    else {
+        echo "No records found.";
+    }
+?>
+        </table>
