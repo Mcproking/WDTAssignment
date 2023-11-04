24ff7abead71e85b9e6e81384828674b0da1fdbf
Eugene Tan <TP068630@mail.apu.edu.my>
2023-11-05 04:54:26 +0800

    Customer List (part 2 still unfinished lol)

--- admin/customerList.php
+++ admin/customerList.php
@@ -44,55 +44,35 @@
                 
             </div>
             <div class="lists" id="seller">
-                <!-- <div id="record">
-                    <div class="record-detail" id="r-id">S2</div>
-                    <div class="record-detail" id="r-user">S1</div>
-                    <div class="record-detail" id="r-email">S1@S.c</div>
-                    <div class="record-detail" id="r-phone">0123456789</div>
-                    <div class="record-detail" id="r-address">mamamiya so looooooooooqweeeqwweqwewqeqweqwooooooooooong, Johor</div>
-                    <div class="record-detail" id="r-action">
-                        <a href="">EDIT</a>
-                        <button>DELETE</button>
-                    </div>
-                </div>
-                <div id="record">
-                    <div class="record-detail" id="r-id">ID</div>
-                    <div class="record-detail" id="r-user">Username</div>
-                    <div class="record-detail" id="r-email">Email</div>
-                    <div class="record-detail" id="r-phone">Phone</div>
-                    <div class="record-detail" id="r-address">Address</div>
-                    <div class="record-detail" id="r-action">
-                        <a href="">EDIT</a>
-                        <button>DELETE</button>
-                    </div>
-                </div> -->
                 <?php
                     include "../php/db_conn.php";
-                    $email_querry = "SELECT `cust_id`,`username`,`email`,`phone_no`,`address`,`state` FROM `customer` CROSS JOIN cust_address ON customer.id = cust_address.cust_id WHERE `username` = ?";
-                    $getAll_querry = "SELECT `cust_id`,`username`,`email`,`phone_no`,`address`,`state` FROM `customer` CROSS JOIN cust_address ON customer.id = cust_address.cust_id";
+                    $email_querry = "SELECT customer.id,`username`,`email`,`phone_no`,`address`,`state` FROM `customer` LEFT JOIN cust_address ON customer.id = cust_address.cust_id WHERE `username` = ? ORDER BY customer.id ASC";
+                    $getAll_querry = "SELECT customer.id,`username`,`email`,`phone_no`,`address`,`state` FROM `customer` LEFT JOIN cust_address ON customer.id = cust_address.cust_id ORDER BY customer.id ASC";
 
                     if(!empty($_GET["searchCustomer"])){
                         if($smst = $conn->prepare($email_querry)){
-                            $smst->bind_param("c", $_GET["searchCustomer"]);
+                            $smst->bind_param("s", $_GET["searchCustomer"]);
                             $smst->execute();
                             $smst->store_result();
 
                             if($smst->num_rows > 0){
-                                $smst->bind_result($id,$username,$email,$phone,$address,$state);
-                                $smst->fetch();
-                                $full_address = $address.$state;
-                                $html = '
-                                <div id="record">
-                                    <div class="record-detail" id="r-id">S'.$id.'</div>
-                                    <div class="record-detail" id="r-user">'.$username.'</div>
-                                    <div class="record-detail" id="r-email">'.$email.'</div>
-                                    <div class="record-detail" id="r-phone">'.$phone.'</div>
-                                    <div class="record-detail" id="r-address">'.$address.'</div>
-                                    <div class="record-detail" id="r-action">
-                                        <a onclick="confirmDelete()" id="delete">DELETE</a>
-                                    </div>
-                                </div>';
-                                echo $html;  
+                                for($x = 0; $x < $smst->num_rows; $x++){
+                                    $smst->bind_result($id,$username,$email,$phone,$address,$state);
+                                    $smst->fetch();
+                                    $full_address = $address.$state;
+                                    $html = '
+                                    <div id="record">
+                                        <div class="record-detail" id="r-id">S'.$id.'</div>
+                                        <div class="record-detail" id="r-user">'.$username.'</div>
+                                        <div class="record-detail" id="r-email">'.$email.'</div>
+                                        <div class="record-detail" id="r-phone">'.$phone.'</div>
+                                        <div class="record-detail" id="r-address">'.$address.'</div>
+                                        <div class="record-detail" id="r-action">
+                                            <a onclick="confirmDelete()" id="delete">DELETE</a>
+                                        </div>
+                                    </div>';
+                                    echo $html;  
+                                }
                             }
                         }
                     }else if($smst = $conn->prepare($getAll_querry)){
@@ -100,20 +80,22 @@
                         $smst->store_result();
 
                         if($smst->num_rows > 0){
-                            $smst->bind_result($id,$username,$email,$phone,$address,$state);
-                            $smst->fetch();
-                            $full_address = $address.', '.$state;
-                            $html = '                                <div id="record">
-                            <div class="record-detail" id="r-id">S'.$id.'</div>
-                            <div class="record-detail" id="r-user">'.$username.'</div>
-                            <div class="record-detail" id="r-email">'.$email.'</div>
-                            <div class="record-detail" id="r-phone">'.$phone.'</div>
-                            <div class="record-detail" id="r-address">'.$full_address.'</div>
-                            <div class="record-detail" id="r-action">
-                                <a onclick="confirmDelete()" id="delete">DELETE</a>
-                            </div>
-                        </div>';
-                            echo $html;
+                            for($x = 0; $x < $smst->num_rows; $x++){
+                                $smst->bind_result($id,$username,$email,$phone,$address,$state);
+                                $smst->fetch();
+                                $full_address = $address.' '.$state;
+                                $html = '<div id="record">
+                                <div class="record-detail" id="r-id">S'.$id.'</div>
+                                <div class="record-detail" id="r-user">'.$username.'</div>
+                                <div class="record-detail" id="r-email">'.$email.'</div>
+                                <div class="record-detail" id="r-phone">'.$phone.'</div>
+                                <div class="record-detail" id="r-address">'.$full_address.'</div>
+                                <div class="record-detail" id="r-action">
+                                    <a onclick="confirmDelete()" id="delete">DELETE</a>
+                                </div>
+                            </div>';
+                                echo $html;
+                            }
                         }
                     }
                 ?>
@@ -138,12 +120,12 @@
             elementItem.classList.remove("navopen");
         }
     </script>
-
+<?php echo $id?>
     <script>
         function confirmDelete() {
             let confirmation = "Are you sure you want to delete this customer?\nPress OK to confirm\nPress Cancel to cancel deletion";
             if (confirm(confirmation) == true) {
-                window.location.href="./deleteSeller.php?SellerID="+$id;
+                window.location.href="./deleteCustomer.php?customerid=<?php echo $id ?>";
             }
         }
     </script>
