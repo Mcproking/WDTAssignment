c155b7e23d44626a25c7cb2dc798ffb6a8a157d9
Mcproking <shanefoomg@gmail.com>
2023-11-04 23:12:18 +0800

    aboutus updated

--- AboutUs.php
+++ AboutUs.php
@@ -3,10 +3,14 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>About Us</title>
     <link rel="stylesheet" href="./static/body.css">
     <link rel="stylesheet" href="./static/includes.css">
     <style>
+        .wrapper{
+            display: flex;
+            flex-direction: column;
+        }
         #description {
             height:110px;
             width:75%;
@@ -48,7 +52,7 @@
             padding:25px;
             text-align: center;
         }
-        img {
+        #management * img {
             height: 225px;
             width: 225px;
         }
@@ -56,13 +60,7 @@
             padding: 10px 10px;
             text-align: center;
             font-size: large;
-        }
-        #footer {
-            float:left;
-            margin: 100px;
-            width: 100%;
-        }
-        
+        }        
         
     </style>
 </head>
@@ -70,26 +68,29 @@
     <?php
     include "./includes/header.php";
     ?>
-    <h1>About Us</h1>
-    <div id="description">
-        Hi! Welcome to EZGG, where you can find all your grocery needs in just one place. How convenient! As part of APU Web Solutions  Sdn. Bhd., we created this website with the intent of delivering basic food needs to your doorstep with just a click of a few buttons. We hope that we can provide assistance with food security issues and achieve sustainable agricultural development.
-    </div>
-    <br>
-    <h1>Management</h1>
-    <div id="management">
-        <div id="management1">
-            <img src="imgs/customer.png" alt="Shane" srcset="">
-            <div id="management_content"><b><u>Director</u></b><br><br>Ho Shane Foong</div>
+    <div class="wrapper">
+        <h1>About Us</h1>
+            <div id="description">
+                Hi! Welcome to EZGG, where you can find all your grocery needs in just one place. How convenient! As part of APU Web Solutions  Sdn. Bhd., we created this website with the intent of delivering basic food needs to your doorstep with just a click of a few buttons. We hope that we can provide assistance with food security issues and achieve sustainable agricultural development.
+            </div>
+            <br>
+            <h1>Management</h1>
+            <div id="management">
+                <div id="management1">
+                    <img src="imgs/customer.png" alt="Shane" srcset="">
+                    <div id="management_content"><b><u>Director</u></b><br><br>Ho Shane Foong</div>
+                </div>
+                <div id="management2">
+                    <img src="imgs/customer.png" alt="Daryl" srcset="">
+                    <div id="management_content"><b><u>Manager</u></b><br><br>Daryl Sim Wei Shern</div>
+                </div>
+                <div id="management3">
+                    <img src="imgs/customer.png" alt="Eugene" srcset="">
+                    <div id="management_content"><b><u>Assitant Manager</u></b><br><br>Eugene Tan Ting Siang</div>
+                </div>
+            </div>
         </div>
-        <div id="management2">
-            <img src="imgs/customer.png" alt="Daryl" srcset="">
-            <div id="management_content"><b><u>Manager</u></b><br><br>Daryl Sim Wei Shern</div>
-        </div>
-        <div id="management3">
-            <img src="imgs/customer.png" alt="Eugene" srcset="">
-            <div id="management_content"><b><u>Assitant Manager</u></b><br><br>Eugene Tan Ting Siang</div>
-        </div>
-    </div>
+    
     <div id="footer">
         <?php
         include "./includes/footer.html";
