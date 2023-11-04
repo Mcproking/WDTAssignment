59e43545ef873b46c3e74bc4eb1656bc02c87fd7
Mcproking <shanefoomg@gmail.com>
2023-11-04 18:10:03 +0800

    sellerlist with querry work, table format change

--- admin/sellerList.php
+++ admin/sellerList.php
@@ -7,9 +7,8 @@
     <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Inter">
     <link rel="stylesheet" href="../static/body.css">
     <link rel="stylesheet" href="../static/includes.css">
-    <link rel="stylesheet" href="../static/admin/admin.css">
+    <link rel="stylesheet" href="../static/admin/sellerList.css">
     <link rel="stylesheet" href="../static/admin/sidenav.css">
-
 </head>
 <body>
     <?php
@@ -23,64 +22,112 @@
             include "./sidenav.html";
             ?>
         </div>
-    </div>
 
-    <div id="items_title">
-        <h1>Seller List</h1>
-    </div>
+        <div id="header-title">
+            Seller List
+        </div>
 
-    <div>
-        <form action="searchSeller.php" method="get" id="Search">
-            Search by Username: <input type="text" name="searchSeller" id="searchSeller" placeholder="Enter Username">
+        <form action="" method="get" id="Search">
+            <div id="form-search-header">Search by Username:</div>
+            <input type="text" name="searchSeller" id="searchSeller" placeholder="Enter Username">
             <button type="submit" id="searchButton" name="searchButton">Search</button>
         </form>
+
+        <div class="list">
+            <div class="list-header" id="header">
+                <div class="header-detail" id="h-id">ID</div>
+                <div class="header-detail" id="h-user">Username</div>
+                <div class="header-detail" id="h-email">Email</div>
+                <div class="header-detail" id="h-phone">Phone</div>
+                <div class="header-detail" id="h-address">Address</div>
+                <div class="header-detail" id="h-action">Action</div>
+                
+            </div>
+            <div class="lists" id="seller">
+                <!-- <div id="record">
+                    <div class="record-detail" id="r-id">S2</div>
+                    <div class="record-detail" id="r-user">S1</div>
+                    <div class="record-detail" id="r-email">S1@S.c</div>
+                    <div class="record-detail" id="r-phone">0123456789</div>
+                    <div class="record-detail" id="r-address">mamamiya so looooooooooqweeeqwweqwewqeqweqwooooooooooong, Johor</div>
+                    <div class="record-detail" id="r-action">
+                        <a href="">EDIT</a>
+                        <button>DELETE</button>
+                    </div>
+                </div>
+                <div id="record">
+                    <div class="record-detail" id="r-id">ID</div>
+                    <div class="record-detail" id="r-user">Username</div>
+                    <div class="record-detail" id="r-email">Email</div>
+                    <div class="record-detail" id="r-phone">Phone</div>
+                    <div class="record-detail" id="r-address">Address</div>
+                    <div class="record-detail" id="r-action">
+                        <a href="">EDIT</a>
+                        <button>DELETE</button>
+                    </div>
+                </div> -->
+                <?php
+                    include "../php/db_conn.php";
+                    $email_querry = "SELECT `id`,`username`,`email`,`phone_no`,`address`,`state` FROM `seller` WHERE `username` = ?";
+                    $getAll_querry = "SELECT `id`,`username`,`email`,`phone_no`,`address`,`state` FROM `seller`";
+
+                    if(!empty($_GET["searchSeller"])){
+                        if($smst = $conn->prepare($email_querry)){
+                            $smst->bind_param("s", $_GET["searchSeller"]);
+                            $smst->execute();
+                            $smst->store_result();
+
+                            if($smst->num_rows > 0){
+                                $smst->bind_result($id,$username,$email,$phone,$address,$state);
+                                $smst->fetch();
+                                $full_address = $address.$state;
+                                $html = '
+                                <div id="record">
+                                    <div class="record-detail" id="r-id">S'.$id.'</div>
+                                    <div class="record-detail" id="r-user">'.$username.'</div>
+                                    <div class="record-detail" id="r-email">'.$email.'</div>
+                                    <div class="record-detail" id="r-phone">'.$phone.'</div>
+                                    <div class="record-detail" id="r-address">'.$address.'</div>
+                                    <div class="record-detail" id="r-action">
+                                        <a href="./editSeller.php?SellerID='.$id.'">EDIT</a>
+                                        <button>DELETE</button>
+                                    </div>
+                                </div>';
+                                echo $html;  
+                            }
+                        }
+                    }else if($smst = $conn->prepare($getAll_querry)){
+                        $smst->execute();
+                        $smst->store_result();
+
+                        if($smst->num_rows > 0){
+                            $smst->bind_result($id,$username,$email,$phone,$address,$state);
+                            $smst->fetch();
+                            $full_address = $address.$state;
+                            $html = '                                <div id="record">
+                            <div class="record-detail" id="r-id">S'.$id.'</div>
+                            <div class="record-detail" id="r-user">'.$username.'</div>
+                            <div class="record-detail" id="r-email">'.$email.'</div>
+                            <div class="record-detail" id="r-phone">'.$phone.'</div>
+                            <div class="record-detail" id="r-address">'.$address.'</div>
+                            <div class="record-detail" id="r-action">
+                                <a href="./editSeller.php?SellerID='.$id.'">EDIT</a>
+                                <button>DELETE</button>
+                            </div>
+                        </div>';
+                            echo $html;
+                        }
+                    }
+                ?>
+
+            </div>
+        </div>
+
+
     </div>
 
     <?php
-    include "../php/db_conn_daryl.php";
-    
-    $displayQuery = "SELECT * FROM seller ORDER BY SellerID ASC LIMIT 20";
-    $displayResults = mysqli_query($conn, $displayQuery);
-          
-    if (mysqli_num_rows($displayResults) > 0) {
-?>
-        <table border="1">
-            <tr>
-                <th>Seller ID</th>
-                <th>Username</th>
-                <th>Email</th>
-                <th>Phone</th>
-                <th>Address</th>
-                <th>State</th>
-                <th colspan="2">Actions</th>
-            </tr>
-
-        <?php
-        while ($row = mysqli_fetch_assoc($displayResults)) {
-            echo "<tr>";
-            echo "<td>" . $row['SellerID'] . "</td>";
-            echo "<td>" . $row['SellerUsername'] . "</td>";
-            echo "<td>" . $row['SellerEmail'] . "</td>";
-            echo "<td>" . $row['SellerPhone'] . "</td>";
-            echo "<td>" . $row['SellerAddress'] . "</td>";
-            echo "<td>" . $row['SellerState'] . "</td>";
-        ?>
-            <td><a class="actionButtons" href="editSeller.php?SellerID=<?php echo $row['SellerID']; ?>">Edit</a></td>
-            <td><a class="actionButtons" href="deleteSeller.php?SellerID=<?php echo $row['SellerID']; ?>">Delete</a></td>
-
-        <?php
-            echo "</tr>";
-        }
-    }
-    else {
-        echo "No records found.";
-    }
-?>
-        </table>
-
-
-    <?php
-        include "../includes/footer.html";
+        // include "../includes/footer.html";
     ?>
 
     <script>
